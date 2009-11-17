0300c2d72b3cd4b508df72b0e73c8f965590a18c
attila.lendvai <attila.lendvai@gmail.com>
2009-11-17 14:52:55 +0000

    follow (def package ...) changes wrt readtable setup

--- source/system.lisp
+++ source/system.lisp
@@ -7,58 +7,6 @@
 (in-package :hu.dwim.asdf)
 
 ;;;;;;
-;;; Readtable support
-
-(defclass readtable-function-mixin ()
-  ((setup-readtable-function-name
-    :initform nil
-    :initarg :setup-readtable-function-name
-    :accessor setup-readtable-function-name)))
-
-(defclass cl-source-file-with-readtable (cl-source-file readtable-function-mixin)
-  ())
-
-(defclass system-with-readtable (system readtable-function-mixin)
-  ())
-
-#+asdf-system-connections
-(defclass system-connection-with-readtable (system-connection readtable-function-mixin)
-  ())
-
-(defmethod asdf::module-default-component-class ((class system-with-readtable))
-  'cl-source-file-with-readtable)
-
-#+asdf-system-connections
-(defmethod asdf::module-default-component-class ((class system-connection-with-readtable))
-  'cl-source-file-with-readtable)
-
-(defun maybe-funcall-setup-readtable-function (system &optional component)
-  (setf system (find-system system)) ; safe to call both with symbols and system instances
-  (let ((setup-readtable-function-name (or (when component
-                                             (setup-readtable-function-name component))
-                                           (setup-readtable-function-name system))))
-    (when setup-readtable-function-name
-      (unless (position #\: (string setup-readtable-function-name) :test #'char=)
-        (error "You probably want to fully qualify your SETUP-READTABLE-FUNCTION-NAME ~S"
-               setup-readtable-function-name))
-      ;; we need to ignore-errors because the setup-readtable-function is only defined
-      ;; after some of the early files are already loaded.
-      ;; FIXME this will intern into random packages because SETUP-READTABLE-FUNCTION-NAME has a default value...
-      (let ((setup-readtable-function (ignore-errors
-                                        (fdefinition (read-from-string setup-readtable-function-name)))))
-        (when setup-readtable-function
-          (funcall setup-readtable-function))))))
-
-(defmethod perform :around ((op operation) (component cl-source-file-with-readtable))
-  (let* ((*readtable* *readtable*)
-         (system (loop :for parent = (component-parent component) :then (component-parent parent)
-                       :while parent
-                       :when (typep parent 'system)
-                       :return parent)))
-    (maybe-funcall-setup-readtable-function system component)
-    (call-next-method)))
-
-;;;;;;
 ;;; Package support
 
 (defclass system-with-package (system)
@@ -74,26 +22,14 @@
           (string-upcase (component-name system)))))
 
 ;;;;;;
-;;; Readtable and package support
-
-(defclass system-with-readtable+package (system-with-readtable system-with-package)
-  ())
-
-(defmethod reinitialize-instance :after ((system system-with-readtable+package) &rest args &key &allow-other-keys)
-  (declare (ignore args))
-  (unless (setup-readtable-function-name system)
-    (setf (setup-readtable-function-name system)
-          (concatenate 'string (string-upcase (system-package-name system)) "::SETUP-READTABLE"))))
-
-;;;;;;
 ;;; DWIM system
 
 (defvar *muffle-optimization-warnings* t)
 
-(defclass hu.dwim.source-file (cl-source-file-with-readtable)
+(defclass hu.dwim.source-file (cl-source-file)
   ())
 
-(defclass hu.dwim.system (system-with-readtable+package)
+(defclass hu.dwim.system (system-with-package)
   ((test-system-name
     :initform nil
     :initarg :test-system-name
@@ -108,17 +44,13 @@
              "Levente Mészáros <levente.meszaros@gmail.com>")
    :licence "BSD / Public domain"))
 
-(defclass hu.dwim.test-system (system-with-readtable+package)
+(defclass hu.dwim.test-system (system-with-package)
   ((test-result
     :initform nil
     :initarg :test-result
     :accessor system-test-result)))
 
-(defclass hu.dwim.documentation-system (system-with-readtable+package)
-  ())
-
-#+asdf-system-connections
-(defclass hu.dwim.system-connection (system-connection-with-readtable)
+(defclass hu.dwim.documentation-system (system-with-package)
   ())
 
 (defmethod reinitialize-instance :after ((system hu.dwim.system) &rest args &key &allow-other-keys)
@@ -130,15 +62,21 @@
     (setf (system-documentation-system-name system)
           (concatenate 'string (string-downcase (component-name system)) ".documentation"))))
 
+(defmethod asdf::module-default-component-class ((class hu.dwim.test-system))
+  'hu.dwim.source-file)
+
 (defmethod asdf::module-default-component-class ((class hu.dwim.system))
   'hu.dwim.source-file)
 
-#+asdf-system-connections
-(defmethod asdf::module-default-component-class ((class hu.dwim.system-connection))
-  'hu.dwim.source-file)
-
 (defmethod perform :around ((op operation) (component hu.dwim.source-file))
-  (let ((*features* *features*))
+  (let ((*features* *features*)
+        (*readtable* (copy-readtable *readtable*))
+        (*package* *package*)
+        (hu.dwim.common-package (find-package :hu.dwim.common)))
+    (when hu.dwim.common-package
+      ;; when the hu.dwim.common package is available, then we read lisp files into that, so that hu.dwim.common:in-package can shadow cl:in-package.
+      ;; see hu.dwim.def/source/extended-package.lisp for more info.
+      (setf *package* hu.dwim.common-package))
     (unless hu.dwim.asdf:*load-as-production?*
       (pushnew :debug *features*))
     (call-in-system-environment op (component-system component) #'call-next-method)))
